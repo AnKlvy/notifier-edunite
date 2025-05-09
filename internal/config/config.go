f61ac86d1411b20169308d8321e6496434b08d5e
Andrey Klimov <andreyklimov.ver2@gmail.com>
2025-05-09 15:20:41 +0500

    fix env db name

--- internal/config/config.go
+++ internal/config/config.go
@@ -35,7 +35,7 @@ func LoadConfig() (*Config, error) {
 	}
 	flag.IntVar(&cfg.Port, "port", 9100, "API server port")
 	flag.StringVar(&cfg.Env, "env", "development", "Environment (development|staging|production)")
-	flag.StringVar(&cfg.Db.Dsn, "db-dsn", os.Getenv("NEWS_SERVICE_DB_DSN"), "PostgreSQL DSN")
+	flag.StringVar(&cfg.Db.Dsn, "db-dsn", os.Getenv("NOTIFY_SERVICE_DB_DSN"), "PostgreSQL DSN")
 	flag.IntVar(&cfg.Db.MaxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.Db.MaxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.Db.MaxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
